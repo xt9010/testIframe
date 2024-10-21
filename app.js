0048a8954854ce30848d5dae97a6987ebf0db020
xt9010 <xt9010@rimac.com.pe>
2024-10-21 12:22:45 -0500

    chore: update with parameters

--- app.js
+++ app.js
@@ -1,15 +1,59 @@
-// app.js
-document.addEventListener("DOMContentLoaded", function () {
-    const paymentForm = document.getElementById("payment-form");
-  
-    // Obtener el parámetro "token" de la URL
+document.addEventListener("DOMContentLoaded", function () {  
+window.addEventListener("message", (event) => {
+      const data = JSON.parse(event?.data);
+      if (!!data?.token) {
+        token = data.token;
+        KR.setFormConfig({ formToken: token });
+      }
+    });
+
     const urlParams = new URLSearchParams(window.location.search);
     const token = urlParams.get("token");
   
-    // Asignar el token al formulario de pago
     if (token) {
-      paymentForm.setAttribute("kr-form-token", token);
+      KR.setFormConfig({ formToken: token });
     }
 
+    KR.onSubmit(function (event) {
+      console.log("El pago ha sido enviado:", event);
+      const isSuccess = event?.clientAnswer?.orderStatus === "PAID";
+      const status = isSuccess? "success" : "error";
+      const message = isSuccess? "Pago realizado con éxito" : "El pago ha fallado. Por favor, inténtelo de nuevo.";
+      const infoToJson = JSON.stringify({
+        status: status,
+        message: message,
+        payload: event?.rawClientAnswer,
+      })
+      console.log({infoToJson})
+      window.ReactNativeWebView.postMessage(
+        infoToJson
+      );
+    });
+
+    
+
+    KR.onSuccess(function (event) {
+      console.log("Pago exitoso:", event);
+      // Envía los resultados al WebView
+      window.ReactNativeWebView.postMessage(
+        JSON.stringify({
+          status: "success",
+          message: "Pago realizado con éxito",
+          payload: event,
+        })
+      );
+    });
+  
+    KR.onError(function (event) {
+      console.log("Error en el pago:", event);
+      // Envía los resultados al WebView en caso de fallo
+      window.ReactNativeWebView.postMessage(
+        JSON.stringify({
+          status: "error",
+          message: "El pago ha fallado. Por favor, inténtelo de nuevo.",
+          payload: event,
+        })
+      );
+    });
   });
   
